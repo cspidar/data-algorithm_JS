e8de782cfaa3cb6fc63a17f6db3fb9cb0e6ef2ee
cspidar <32433348+cspidar@users.noreply.github.com>
2021-04-21 19:51:08 +0900

    1

--- @Quiz.js
+++ @Quiz.js
@@ -1,25 +1,22 @@
 //
-function sol(n1, n2) {
-  //
-  //
-  const arr = [];
-  for (i = 0; i < n1; i++) {
-    arr.push(i + 1);
-  }
-
-  let pos = 0;
-  let who = 0;
-  while (arr.length <= 1) {
-    if (pos == arr.length) {
-      pos = 0;
+function sol(str1, str2) {
+  let rarr = '';
+  for (i = 0; i < str2.length; i++) {
+    if (str2[i] == 'C' || str2[i] == 'A' || str2[i] == 'B') {
+      rarr += str2[i];
     }
   }
 
-  return arr;
+  let ans = '';
+  if (rarr == str1) {
+    ans = 'YES';
+  } else ans = 'NO';
+
+  return ans;
 }
 
-const in1 = 8;
-const in2 = 3;
+const in1 = 'CBA';
+const in2 = 'CBDAGE';
 
 console.log(sol(in1, in2));
 !console.table(sol(in1, in2));
