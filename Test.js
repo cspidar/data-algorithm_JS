72408a99874eb7a102ac7e0c608752f68bd489b0
cspidar <32433348+cspidar@users.noreply.github.com>
2022-04-20 01:43:42 +0900

    1

--- Test.js
+++ Test.js
@@ -1,28 +1,4 @@
-let arr = [6, 11, 'ㅁ', 'A', 1];
+let arr = ['c', 'a', , 'z', 'k'];
 
-function arrPr(arr, r) {
-  let res = [];
-  // let cnt = 0;
-  let ch = Array.from({ length: arr.length }, () => 0);
-  let tmp = Array.from({ length: r }, () => 0);
-  function DFS(L) {
-    if (L === r) {
-      res.push(tmp.slice());
-      // cnt++;
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        if (ch[i] === 0) {
-          ch[i] = 1;
-          tmp[L] = arr[i];
-          DFS(L + 1);
-          ch[i] = 0;
-        }
-      }
-    }
-  }
-  DFS(0);
-  return res;
-  // return cnt;
-}
-
-console.log(arrPr(arr, 2));
+let res = arr.join('');
+console.log(res);
