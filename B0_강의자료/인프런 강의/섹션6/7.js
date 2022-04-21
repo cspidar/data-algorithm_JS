3689c2e948e6d00ecbb0eca4054d391c07b3b1c6
Cspidar <cspidar@gmail.com>
2022-04-21 16:54:40 +0900

    1

---
+++ B0_강의자료/인프런 강의/섹션6/7.js
@@ -0,0 +1,6 @@
+let m = 32;
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+
+let sort_arr = arr.sort((a, b) => b - a);
+
+console.log(sort_arr);
--- Test.js
+++ Test.js
@@ -1,24 +1,24 @@
-let w = 8;
-let h = 12;
-let answer = 0;
+let m = 32;
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+arr = arr.sort((a, b) => b - a);
 
-let getGCD = (num1, num2) => {
-  let gcd = 1;
+// console.log(arr);
 
-  for (let i = 2; i <= Math.min(num1, num2); i++) {
-    if (num1 % i === 0 && num2 % i === 0) {
-      gcd = i;
-    }
+let lt = arr[0];
+let rt = arr[arr.length - 1];
+let res;
+while (lt >= rt) {
+  let mid = Math.floor((lt + rt) / 2);
+  if (mid === m) {
+    // 답의 조건
+    res = mid; // 현상황 답 저장
+    break;
   }
+  if (mid > m) {
+    lt = mid + 1; // mid는 이미 확인했기때문에 rt = mid - 1 / lt = mid + 1
+  } else if (mid < m) {
+    rt = mid - 1;
+  }
+}
 
-  return gcd;
-};
-
-let gcd = getGCD(w, h);
-
-let ratio = w / h;
-if (ratio > 1) ratio = 1 / ratio;
-let gap = (w / gcd) * (h / gcd) * ratio;
-answer = w * h - gap;
-
-console.log(answer);
+console.log(res);
